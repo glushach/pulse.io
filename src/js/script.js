d4bbe584a302ee169929706e79f1a35116806129
Alex <shoppingplaneta@gmail.com>
2020-06-04 13:53:25 +0300

    Свертанная и адаптированная под mobile carousel

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false,
+  responsive: {
+    320: {
+      nav: true
+    },
+    767: {
+      nav: true
+    }
+  }
+});
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
+
