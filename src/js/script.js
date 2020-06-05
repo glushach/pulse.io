6c039b5e1c82675672420bfaa11723479f143c74
Alex <shoppingplaneta@gmail.com>
2020-06-05 16:37:42 +0300

    Сверстаны табы без адаптаци под mobile

--- src/js/script.js
+++ src/js/script.js
@@ -4,12 +4,11 @@ const slider = tns({
   slideBy: 'page',
   autoplay: false,
   controls: false,
-  nav: false,
   responsive: {
-    320: {
+    767: {
       nav: true
     },
-    767: {
+    320: {
       nav: true
     }
   }
@@ -19,5 +18,39 @@ document.querySelector('.prev').addEventListener('click', function () {
 });
 document.querySelector('.next').addEventListener('click', function () {
   slider.goTo('next');
+});
+
+$(document).ready(function(){
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  // $('.catalog-item__link').each(function(i) {
+  //   $(this).on('click', function(e) {
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  //   });
+  // });
+  // $('.catalog-item__back').each(function(i) {
+  //   $(this).on('click', function(e) {
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  //   });
+  // });
+  function toggleClider(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+  }
+  toggleClider('.catalog-item__link');
+  toggleClider('.catalog-item__back');
 });
 
