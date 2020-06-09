8ec9eee576dc6289df3eb1f7d03b136dfc74291f
Alex <shoppingplaneta@gmail.com>
2020-06-09 19:15:05 +0300

    Созданны модельные окна

--- src/js/script.js
+++ src/js/script.js
@@ -53,5 +53,24 @@ $(document).ready(function(){
   }
   toggleClider('.catalog-item__link');
   toggleClider('.catalog-item__back');
+
+  // Модальные окна
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  });
+
+  // $('.button_mini').on('click', function() {
+  //   $('.overlay, #order').fadeIn('slow');
+  // });
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
+  });
 });
 
